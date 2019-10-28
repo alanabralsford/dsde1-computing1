fe6b5762680e7809c1f61c3ad0da049b20dd5a14
alanabralsford <alana.bralsford19@imperial.ac.uk>
2019-10-28 00:26:35 +0000

    3 guesses

---
+++ Week-3/firstProgram.py
@@ -0,0 +1,27 @@
+message = 'if this has printed, code has been executed successfully'
+print(message)
+x = 0
+print("what is your message?")
+r = input('-->')
+print(r)
+print("guess a number between 1 and 10")
+guess = input('-->')
+guess = int(guess)
+import random
+roll = random.randint(1,10)
+if (roll == guess):
+    print(roll == guess)
+else:
+    while x < 2:
+        print(roll) '''in a real situation, take this line out as it tells you the generated number, I kept this to test the game
+        print("guess a number between 1 and 10")
+        guess = input('-->')
+        guess = int(guess)
+        if (roll == guess):
+            print(roll == guess)
+            break
+        print(roll == guess)
+        x = x + 1
+
+#print(roll == guess)
+#print(roll)
--- Week-3/toys.py
+++ Week-3/toys.py
@@ -9,20 +9,25 @@ with functions.
 # write a function that adds 1
 # to the input and prints the result
 def inc(a):
+    a = a + 1
     print(a)
-
+#print(inc(5))
+inc(5)
 
 # write a function that adds 1
 # to the input and returns the result
 def inc_return(a):
-    return # hint this is incomplete
-
+    a = a + 1
+    return a
+print(inc_return(12))
 
 # write a function that adds
 # the two input numbers together
 # and returns the sum
 def sum(a, b):
-    return
+    sum = a + b
+    return sum
+print(sum(5, 3))
 
 
 # write a function that takes two
@@ -30,14 +35,21 @@ def sum(a, b):
 # sum() and then increments the sum
 # using inc_return
 def sum_inc(a, b):
-    return
+    sum2 = sum(a, b)
+    sum2 = inc_return(sum2)
+
+    return sum2
+
+print(sum_inc(2,4))
 
 
 # write a function that returns a 
 # boolean (True or False) for whether 
 # the input number is even
 def is_even(a):
-    return
+    boolTest =  (a % 2) == 0
+    return boolTest
+print(is_even(5))
 
 
 # create for loop that takes a string
@@ -47,7 +59,26 @@ def is_even(a):
 # e.g. string_repeat('ho', 3) returns
 # 'hohoho'
 def string_repeat(phrase, repeat):
+    s = str()
+    for i in range(repeat):
+        s += phrase
+    print(s)
+    #for i in range(repeat)
+        #string = phrase * repeat
+    
+    #print string
+string_repeat("dog", 4)
+
+def main()
+    # input function you want to test
+
+if __name__ = '__main__':
+    main()
+    #in the terminal, its studying internal variable in python, executing from command line not interactive shell
+
+
+    #return (phrase * (int(length/len(phrase))+1))[:repeat]
     # hint: you can add strings together 
     # in order to concatenate them
-    return
+#print(string_repeat(dog, 5))
 
