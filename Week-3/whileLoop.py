4a33317cb31e28657528399f7ae43fcb479c4643
alanabralsford <alana.bralsford19@imperial.ac.uk>
2019-10-28 22:17:23 +0000

    fizz buzz game!!!

--- Week-3/whileLoop.py
+++ Week-3/whileLoop.py
@@ -6,3 +6,16 @@ down = 10
 while down >= 0:
     print(down)
     down = down - 1
+
+for num in range(100):
+    if num % 3 == 0:
+        if num % 5 == 0:
+            print('fizz-buzz')
+        else:
+            print('fizz')
+    else:
+        if num % 5 == 0:
+            print('buzz')
+        else:
+            print(num)
+        
